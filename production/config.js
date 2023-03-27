edc908b3c28a0beab6194bb7317f4cc183c39d70
poncj <91033092+poncj@users.noreply.github.com>
2023-03-28 02:05:53 +0600

    Add files via upload

---
+++ production/config.js
@@ -0,0 +1,6 @@
+export const config = Object.freeze({
+    RPC_URL: "https://data-seed-prebsc-1-s1.binance.org:8545",
+    CONTRACT_ADDRESS: "0x6D67DcfE493FEcB43b7060944cA0E592260E918f",
+    TESTNET_ID: 97,
+    BASE_GAS_LIMIT: 70000
+});
